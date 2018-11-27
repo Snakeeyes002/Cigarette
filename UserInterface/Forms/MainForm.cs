c1b385ded555a992ad1ef67970cd59a08d0bc59c
Snakeeyes002 <38522827+Snakeeyes002@users.noreply.github.com>
2018-11-27 10:49:12 +0200

    Log info on main form loading

--- UserInterface/Forms/MainForm.cs
+++ UserInterface/Forms/MainForm.cs
@@ -20,6 +20,7 @@ namespace UserInterface.Forms
 
         private async void MainForm_Load(object sender, EventArgs e)
         {
+         System.Diagnostics.Debug.WriteLine("Main");
             WebClient client = new WebClient();
             string result = await client.DownloadStringTaskAsync("https://jsonplaceholder.typicode.com/posts");
 
